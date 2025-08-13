698938e31016221608d63166004adb4d18932d70
Ishit <ishitmittal31@gmail.com>
2025-08-13 23:42:45 +0530

    First Program

---
+++ Assignment1/Ques1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World!\n";
+    cout<<"This is my First Program"<<endl;
+    cout<<"\t My Age is 18"<<endl;
+    cout<<"This will trigger a beep sound!\a\n";
+    return 0;
+}
